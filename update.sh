a3c128e56bd21e0bb2fdc41d584a135bc003bd7c
Noel Hibbard <Noel.Hibbard@jaru.com>
2022-10-06 10:28:23 -0400

    cleaning build env before updating; switching docker image tag to :pulse; Fixing sed bug when adding --with-pa

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+SHAIRPORT_SYNC_BRANCH=development
+NQPTP_BRANCH=development
+
 needsupdate=0
 
 #check to see if a new shairport-sync version is available
 cd ./shairport-sync
-git reset --hard > /dev/null 2>&1
-git fetch > /dev/null 2>&1
+git checkout $SHAIRPORT_SYNC_BRANCH --quiet
+git reset --hard --quiet
+git fetch --quiet
 status=$(git status -sb --porcelain=v2)
 regex="branch.ab \+([0-9]*) -([0-9]*)"
 if [[ $status =~ $regex ]]
@@ -15,15 +19,16 @@ then
     if [ $behind -gt 0 ]
     then
         needsupdate=1
-        git pull > /dev/null 2>&1
+        git pull --quiet
     fi
 fi
 cd ..
 
 #check to see if a new nqptp version is available
 cd ./nqptp
-git reset --hard > /dev/null 2>&1
-git fetch > /dev/null 2>&1
+git checkout $NQPTP_BRANCH --quiet
+git reset --hard --quiet
+git fetch --quiet
 status=$(git status -sb --porcelain=v2)
 regex="branch.ab \+([0-9]*) -([0-9]*)"
 if [[ $status =~ $regex ]]
@@ -33,7 +38,7 @@ then
     if [ $behind -gt 0 ]
     then
         needsupdate=1
-        git pull > /dev/null 2>&1
+        git pull --quiet
     fi
 fi
 cd ..
@@ -41,14 +46,12 @@ cd ..
 if [ $needsupdate -gt 0 ]
 then
     cd ./shairport-sync
-    sed -i 's/RUN .\/configure /RUN .\/configure --with-pa /' ./docker/Dockerfile
+    sed -i 's/.\/configure --/.\/configure --with-pa --/' ./docker/Dockerfile
     sed -i 's/git \\/git \\\n        pulseaudio-dev \\/' ./docker/Dockerfile
     sed -i 's/alsa-lib \\/alsa-lib \\\n        pulseaudio \\\n        pulseaudio-utils \\\n        curl \\/' ./docker/Dockerfile
 
-    SHAIRPORT_SYNC_BRANCH=development
-    NQPTP_BRANCH=development
-
-    docker buildx build -f ./docker/Dockerfile --no-cache --build-arg SHAIRPORT_SYNC_BRANCH=$SHAIRPORT_SYNC_BRANCH --build-arg NQPTP_BRANCH=$NQPTP_BRANCH -t shairport-sync:unstable-development .
+    make distclean > /dev/null 2>&1
+    docker buildx build -f ./docker/Dockerfile --no-cache --build-arg SHAIRPORT_SYNC_BRANCH=$SHAIRPORT_SYNC_BRANCH --build-arg NQPTP_BRANCH=$NQPTP_BRANCH -t shairport-sync:pulse .
 
     cd ..
     docker-compose up -d
