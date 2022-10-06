35fc79273674424ff9a83341e4bbb815c57d87bd
Noel Hibbard <Noel.Hibbard@jaru.com>
2022-10-06 10:37:17 -0400

    Forgot to also apply these changes to build-sps-docker-image.sh

--- build-sps-docker-image.sh
+++ build-sps-docker-image.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/mikebrady/nqptp -b development
-git clone https://github.com/mikebrady/shairport-sync -b development
-cd ./shairport-sync
-
-sed -i 's/RUN .\/configure /RUN .\/configure --with-pa /' ./docker/Dockerfile
-sed -i 's/git \\/git \\\n        pulseaudio-dev \\/' ./docker/Dockerfile
-sed -i 's/alsa-lib \\/alsa-lib \\\n        pulseaudio \\\n        pulseaudio-utils \\\n        curl \\/' ./docker/Dockerfile
-
 SHAIRPORT_SYNC_BRANCH=development
 NQPTP_BRANCH=development
 
-docker buildx build -f ./docker/Dockerfile --no-cache --build-arg SHAIRPORT_SYNC_BRANCH=$SHAIRPORT_SYNC_BRANCH --build-arg NQPTP_BRANCH=$NQPTP_BRANCH -t shairport-sync:unstable-development .
+git clone https://github.com/mikebrady/nqptp -b $SHAIRPORT_SYNC_BRANCH
+git clone https://github.com/mikebrady/shairport-sync -b $NQPTP_BRANCH
+cd ./shairport-sync
+
+sed -i 's/configure --/configure --with-pa --/' ./docker/Dockerfile
+sed -i 's/git \\/git \\\n        pulseaudio-dev \\/' ./docker/Dockerfile
+sed -i 's/alsa-lib \\/alsa-lib \\\n        pulseaudio \\\n        pulseaudio-utils \\\n        curl \\/' ./docker/Dockerfile
+
+make distclean > /dev/null 2>&1
+docker buildx build -f ./docker/Dockerfile --no-cache --build-arg SHAIRPORT_SYNC_BRANCH=$SHAIRPORT_SYNC_BRANCH --build-arg NQPTP_BRANCH=$NQPTP_BRANCH -t shairport-sync:pulse .
